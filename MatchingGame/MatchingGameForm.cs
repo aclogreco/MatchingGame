67caa33e7381701762c4b47192a0a818a079023a
Anthony LoGreco <aclogreco@gmail.com>
2013-02-24 12:03:39 -0500

    Finish adding a random object and a list of icons.

--- MatchingGame/MatchingGameForm.cs
+++ MatchingGame/MatchingGameForm.cs
@@ -10,6 +10,16 @@ using System.Windows.Forms;
 
 namespace MatchingGame {
 	public partial class MatchingGameForm : Form {
+		// Use this random object to choose random items for the squares.
+		Random random = new Random();
+
+		// Each of these letters is an interesting icon in the Webdings font,
+		// and each letter appears twice in the list.
+		List<string> icons = new List<string>() {
+			"!", "!", "N", "N", ",", ",", "k", "k",
+			"b", "b", "v", "v", "w", "w", "z", "z"
+		};
+
 		public MatchingGameForm () {
 			InitializeComponent();
 		}
