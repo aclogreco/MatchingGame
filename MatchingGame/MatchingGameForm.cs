99a199cf831bd1b7ec0af6eca649877771875ae2
Anthony LoGreco <aclogreco@gmail.com>
2013-06-02 21:19:28 -0400

    Add call to AssignIconsToSquares in the form constructor .

--- MatchingGame/MatchingGameForm.cs
+++ MatchingGame/MatchingGameForm.cs
@@ -34,7 +34,7 @@ namespace MatchingGame {
 				if (iconLabel != null) {
 					int randomNumber = random.Next(icons.Count);
 					iconLabel.Text = icons[randomNumber];
-					// iconLabel.ForeColor = iconLabel.BackColor;
+					iconLabel.ForeColor = iconLabel.BackColor;
 					icons.RemoveAt(randomNumber);
 				}
 			}
@@ -42,6 +42,7 @@ namespace MatchingGame {
 
 		public MatchingGameForm () {
 			InitializeComponent();
+			AssignIconsToSquares();
 		}
 	}
 }
