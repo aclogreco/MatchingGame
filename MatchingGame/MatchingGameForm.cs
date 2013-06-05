2750acdb94e12bce21f02c2352b808dd9071e53b
Anthony LoGreco <aclogreco@gmail.com>
2013-06-04 22:47:10 -0400

    Add click event handler to all the labels. Reveal icon when label is clicked.

--- MatchingGame/MatchingGameForm.cs
+++ MatchingGame/MatchingGameForm.cs
@@ -44,5 +44,24 @@ namespace MatchingGame {
 			InitializeComponent();
 			AssignIconsToSquares();
 		}
+
+		/// <summary>
+		/// Every label's Click event is handled by this event handler
+		/// </summary>
+		/// <param name="sender">The label that was clicked</param>
+		/// <param name="e"></param>
+		private void label_Click (object sender, EventArgs e) {
+			Label clickedLabel = sender as Label;
+
+			if (clickedLabel != null) {
+				// If the clicked label is black, the player clicked
+				// an icon that's already been revealed --
+				// ingnore the click
+				if (clickedLabel.ForeColor == Color.Black)
+					return;
+
+				clickedLabel.ForeColor = Color.Black;
+			}
+		}
 	}
 }
